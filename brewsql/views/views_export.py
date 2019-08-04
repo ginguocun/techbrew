a65af8555c8dfdaa7aac66676cc223c8d9c4c84b
金国群 <guoqun.jin@hotmail.com>
2019-08-04 09:02:42 +0800

    数据导出功能优化

--- brewsql/views/views_export.py
+++ brewsql/views/views_export.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from xlwt import *
-# import os
 from io import BytesIO
 from django.contrib.auth.decorators import login_required, permission_required
 from django.views.decorators.http import require_http_methods
@@ -31,7 +30,15 @@ def excel_export(request, file_name='test', model=None, list_obj=None, head=None
         for obj in list_obj:
             if fields:
                 for d in fields:
-                    w.write(excel_row, k, '{0}'.format(getattr(obj, d)),)
+                    v_list = d.split('__')
+                    value = obj
+                    for v in v_list:
+                        value = getattr(value, v)
+                    if value is True:
+                        value = '是'
+                    if value is False:
+                        value = '否'
+                    w.write(excel_row, k, '{0}'.format(value),)
                     k += 1
             excel_row += 1
             k = 0
@@ -57,9 +64,9 @@ def brew_export(request):
         list_obj = model.objects.filter(date_start__gte=validate_date(s))
         if validate_date(e):
             list_obj = list_obj.filter(date_start__lte=validate_date(e))
-    head = ['id', u'产品名称ID', u'产品名称', u'设备ID', u'设备', u'日期', u'操作人员ID', u'操作人员', u'备注', u'创建时间',
+    head = ['id', u'产品名称', u'设备', u'日期', u'操作人员', u'备注', u'创建时间',
             u'最后更新']
-    fields = ['pk', 'product_name_id', 'product_name', 'tank_id', 'tank', 'date_start', 'operator_id', 'operator',
+    fields = ['pk', 'product_name', 'tank', 'date_start', 'operator',
               'notes', 'datetime_created', 'datetime_updated']
     return excel_export(request, file_name='brew', model=model, list_obj=list_obj, head=head, fields=fields)
 
@@ -76,10 +83,11 @@ def pack_export(request):
         list_obj = Pack.objects.filter(pack_date__gte=validate_date(s))
         if validate_date(e):
             list_obj = list_obj.filter(pack_date__lte=validate_date(e))
-    head = ['id', u'灌装日期', u'灌装批次', u'生产批次ID', u'生产批次', u'产品ID', u'产品', u'数量',
-            u'灌装开始', u'灌装结束', u'备注', u'创建时间', u'最后更新']
-    fields = ['pk', 'pack_date', 'pack_batch_code', 'brew_id', 'brew', 'product_id', 'product', 'pack_num',
-              'pack_start', 'pack_end', 'notes', 'datetime_created', 'datetime_updated']
+    head = ['id', u'灌装日期', u'灌装批次', u'生产批次', u'产品名称', u'包装规格', u'规格单位', u'数量',
+            u'灌装开始', u'灌装结束', u'灌装人员', u'备注', u'创建时间', u'最后更新']
+    fields = ['pk', 'pack_date', 'pack_batch_code', 'brew__brew_batch_code', 'product__product_name',
+              'product__product_pack__product_pack_size', 'product__product_pack__product_pack_unit',
+              'pack_num', 'pack_start', 'pack_end', 'employee', 'notes', 'datetime_created', 'datetime_updated']
     return excel_export(request, file_name='pack', model=model, list_obj=list_obj, head=head, fields=fields)
 
 
@@ -95,8 +103,11 @@ def sale_export(request):
         list_obj = Sale.objects.filter(sale_date__gte=validate_date(s))
         if validate_date(e):
             list_obj = list_obj.filter(sale_date__lte=validate_date(e))
-    head = ['id', u'销售日期', u'销售单号ID', u'销售单号', u'灌装批次ID', u'灌装批次', u'数量', u'销售价格ID', u'销售价格',
-            u'备注', u'正常出售', u'是否有效', u'是否确认', u'创建时间', u'最后更新']
-    fields = ['pk', 'sale_date', 'sale_order_id', 'sale_order', 'pack_id', 'pack', 'sale_num', 'sale_price_link_id',
-              'sale_price', 'notes', 'is_sale', 'is_active', 'is_confirmed', 'datetime_created', 'datetime_updated']
+    head = ['id', u'销售日期', u'销售单号', u'灌装批次', u'产品名称', u'包装规格', u'规格单位', u'数量', u'销售价格',
+            u'销售员', u'备注', u'正常出售', u'是否有效', u'是否确认', u'已收款', u'创建时间', u'最后更新']
+    fields = ['pk', 'sale_date', 'sale_order', 'pack__pack_batch_code', 'pack__product__product_name',
+              'pack__product__product_pack__product_pack_size', 'pack__product__product_pack__product_pack_unit',
+              'sale_num', 'sale_price', 'sale_order__employee',
+              'notes', 'is_sale', 'is_active', 'is_confirmed', 'fee_received', 'datetime_created', 'datetime_updated']
     return excel_export(request, file_name='sale', model=model, list_obj=list_obj, head=head, fields=fields)
+
